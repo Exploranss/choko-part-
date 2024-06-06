7b31dc872d4b5d60e59f5106d722bf0a3c440185
Exploranss <makovskiy04@bk.ru>
2024-06-06 12:39:48 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+
+
+let items = [];
+
+function toggleItem(btn, itemId, price) {
+    let item = items.find(i => i.id === itemId);
+    if (!item) {
+        let newItem = { id: itemId, price: price };
+        items.push(newItem);
+        btn.classList.add('added-to-cart');
+        btn.innerText = "Удалить из корзины";
+        let totalPrice = items.reduce((total, item) => total + item.price, 0);
+        if (totalPrice > 0) {
+            tg.mainButton.setText('Общая цена товаров: ${totalPrice}');
+            if (!tg.mainButton.isVisible) {
+                tg.mainButton.show();
+            }
+        } else {
+            tg.mainButton.hide();
+        }
+    } else {
+        let index = items.indexOf(item);
+        items.splice(index, 1);
+        btn.classList.remove('added-to-cart');
+        btn.innerText = "Добавить в корзину";
+        let totalPrice = items.reduce((total, item) => total + item.price, 0);
+        if (totalPrice > 0) {
+            tg.mainButton.setText('Общая цена товаров: ${totalPrice}');
+            if (!tg.mainButton.isVisible) {
+                tg.mainButton.show();
+            }
+        } else {
+            tg.mainButton.hide();
+        }
+    }
+}
+Telegram.WebApp.onEvent("mainButtonClicked", function() {
+    let data = {
+        items: items,
+        totalPrice: calculateTotalPrice()
+    };
+    tg.sendData(JSON.stringify(data));
+});
+
+function calculateTotalPrice() {
+    return items.reduce((total, item) => total + item.price, 0);
+}
+
+document.getElementById("btn1").addEventListener("click", function() {
+    toggleItem(this, 'item1', 450);
+});
+document.getElementById("btn2").addEventListener("click", function() {
+    toggleItem(this, 'item2', 200);
+});
+document.getElementById("btn3").addEventListener("click", function() {
+    toggleItem(this, 'item3', 300);
+});
+document.getElementById("btn4").addEventListener("click", function() {
+    toggleItem(this, 'item4', 400);
+});
+document.getElementById("btn5").addEventListener("click", function() {
+    toggleItem(this, 'item5', 500);
+});
+document.getElementById("btn6").addEventListener("click", function() {
+    toggleItem(this, 'item6', 600);
+});
---
+++ main.py
@@ -0,0 +1,43 @@
+import asyncio
+import json
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
+
+
+bot = Bot('7317590523:AAE0JAOK5AJonosZMvIDlfM5rxTS43bTeKw')
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start(message: types.Message, state: FSMContext):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, keyboard=[
+        [types.KeyboardButton(text='Выбрать товар', web_app=WebAppInfo(url='https://exploranss.github.io /'))]
+    ])
+    await message.answer("Добро пожаловать, купите пасту!", reply_markup=markup)
+
+@dp.message()
+async def web_app(callback_query):
+    json_data = callback_query.web_app_data.data
+    parsed_data = json.loads(json_data)
+    message = ""
+    for i, item in enumerate(parsed_data['items'], start=1):
+        position = int(item['id'].replace('item', ''))
+        message += f"Позиция: {position}\n"
+        message += f"Стоимость: {item['price']}\n\n"
+
+    message += f"Общая стоимость товаров: {parsed_data['totalPrice']}"
+
+    await bot.send_message(callback_query.from_user.id,f"""
+{message}
+""")
+
+    await bot.send_message('-4245618055', f"""
+новый заказ
+{message}
+    """)
+async def main():
+    await dp.start_polling(bot)
+if __name__ == '__main__':
+    asyncio.run(main())
