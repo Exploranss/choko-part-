0a5ab7503686f54281f725e5b93b94f8bfedf5ae
Exploranss <makovskiy04@bk.ru>
2024-06-22 18:50:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function toggleItem(btn, itemId, price) {
         btn.innerText = "Удалить из корзины";
         let totalPrice = items.reduce((total, item) => total + item.price, 0);
         if (totalPrice > 0) {
-            tg.MainButton.setText('Сделать заказ');
+            tg.MainButton.setText('Цена товаров: ${totalPrice}');
             if (!tg.MainButton.isVisible) {
                 tg.MainButton.show();
             }
